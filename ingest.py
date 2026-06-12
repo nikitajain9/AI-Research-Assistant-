e5a75be1996d2f82df46d06a64304d84588cd4f0
Nikita Jain <nikita.jain982004@gmail.com>
2026-06-12 08:07:10 +0530

    Improving Chunking

--- ingest.py
+++ ingest.py
@@ -2,7 +2,7 @@
 import os
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
-from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_text_splitters import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 
 with open('notes.txt','r') as f:
@@ -22,12 +22,19 @@ splitter = MarkdownHeaderTextSplitter(
 
 chunks = splitter.split_text(text)
 
+splitter2 = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+
+final_chunks = splitter2.split_documents(chunks)
+
 embedding_model = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en-v1.5"
 )
 
 vector_store = Chroma.from_documents(
-    documents = chunks,
+    documents = final_chunks,
     embedding = embedding_model,
     persist_directory="vectorstore"
 )
